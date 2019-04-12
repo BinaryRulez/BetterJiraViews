205b18ecff9eac5ae10e618b6422f2f1d6a41b01
BinaryRulez <benoit.fernandez@gmail.com>
2019-04-12 07:04:26 +0000

    first version of a working test

--- betterjiraviews.js
+++ betterjiraviews.js
@@ -1,1 +1,39 @@
-alert("I'm alive !!!")
+//https://cdn.jsdelivr.net/gh/BinaryRulez/BetterJiraViews/betterjiraviews.js
+
+getResource(ext_jira_base_url + "/rest/api/2/search?jql=filter=91860", function(obj) {
+    txt = ""
+    obj.issues.forEach(function(element) {
+        txt = txt + "<a href='" + ext_jira_base_url + "/browse/" + element.key + "'>" + element.key + "</a>" + "<br>"
+    })
+    document.getElementById("feature1").innerHTML = txt;
+});
+
+function getResource(url, callback) {
+
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Basic '
+        + window.btoa(unescape(encodeURIComponent(ext_user + ":" + ext_user))));
+
+    var myInit = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        credentials: 'include'
+    };
+    fetch(ext_confluence_url + url, myInit)
+        .then(function(response) {
+            console.log("response=");
+            console.log(response);
+            return response.text();
+        })
+        .then(function(text) {
+            console.log("json=" + text);
+
+            var obj = JSON.parse(text);
+            console.log(obj);
+            callback(obj);
+
+            //return obj;
+            //document.getElementById("content").innerHTML = obj.fields.summary;
+        });
+}
